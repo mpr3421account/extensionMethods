6c09bcda7fb781eb37d5023af7250b0113846ddc
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-02-06 08:45:29 -0300

    Demo1 - extensions

---
+++ extensionMethods/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace extensionMethods.Extensions
+{
+    internal static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime dateTime) 
+        {
+            TimeSpan duration = DateTime.Now.Subtract(dateTime);
+            if (duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " hours";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " days";
+            }
+        }
+    }
+}
--- extensionMethods/Program.cs
+++ extensionMethods/Program.cs
@@ -1,1 +1,4 @@
-﻿DateTime dt = new DateTime();
+﻿using extensionMethods.Extensions;
+
+DateTime dt = new DateTime(2023,02,06,8,10,45);
+Console.WriteLine(dt.ElapsedTime());
